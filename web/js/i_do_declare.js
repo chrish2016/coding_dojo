506dcb1d6e01471d6038ce2e04136bc7ad9b2156
Chris Hamilton <chris_hamilton11@yahoo.com>
2018-06-05 16:48:50 -0700

    JavaScript: I do Declare

---
+++ web/js/i_do_declare.js
@@ -0,0 +1,13 @@
+var age = 42;
+var weight = 185;
+var height = 67.5;
+var eyes = 4;
+
+var songs = "manny, moe, and jack";
+var bands = "die kreuzen, black flag, battalion of saints";
+var colors = "black and blue";
+
+var happy = true;
+var sad = false;
+
+console.log(age, weight, height, eyes, songs, bands, colors, happy, sad);
