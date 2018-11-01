92d3bb608e528718b2cc4b9d2811116afe0ac447
Chris Hamilton <chris@Chriss-iMac.local>
2018-11-01 11:56:25 -0700

    nada

---
+++ ios/1_fundamentals/introSwift/introSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  introSwift
+//
+//  Created by Chris Hamilton on 11/1/18.
+//  Copyright © 2018 Chris Hamilton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+       print("Get Funky!")
+    }
+
+
+}
+
